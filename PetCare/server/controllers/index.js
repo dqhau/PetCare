f4d575b3ee38cb06ad8bd489f5c4506363631895
Dao Quang Hau <130766235+dqhau@users.noreply.github.com>
2025-05-17 21:11:59 +0700

    Delete PetCare/server directory

--- PetCare/server/controllers/index.js
+++
@@ -1,9 +0,0 @@
-import petController from "./pets.js";
-import userController from "./users.js";
-import serviceController from "./service.js"
-
-export {
-  petController,
-  userController,
-  serviceController
-};
